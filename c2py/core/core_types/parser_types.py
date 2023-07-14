fad8372a8ffb49eb7d61a938321e583c6d095f4e
nanoric <nanoric@qq.com>
2023-07-14 14:57:35 +0800

    Added files to CxxTypes, making it possible to generate implementations(function body).

--- c2py/core/core_types/parser_types.py
+++ c2py/core/core_types/parser_types.py
@@ -26,9 +26,9 @@ class Symbol:
 
     @property
     def full_name(self):
-        if self.parent is None or self.parent.full_name == '':
+        if self.parent is None or self.parent.full_name == "":
             return f"{self.name}"
-        return f'{self.parent.full_name}::{self.name}'
+        return f"{self.parent.full_name}::{self.name}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}@{self.full_name}"
@@ -51,7 +51,7 @@ class Variable(Symbol):
     static: bool = False
     value: Any = None
     literal: str = None
-    access: str = 'public'  # for class member
+    access: str = "public"  # for class member
 
 
 @dataclass(repr=False)
@@ -61,10 +61,13 @@ class Function(Symbol):
     args: List[Variable] = field(default_factory=list)
     calling_convention: str = "__cdecl"
 
+    # impl
+    body: Optional[list[any]] = field(default_factory=list)
+
     @property
     def address(self):
         """function itself doesn't know whether it is overloaded."""
-        return f'&{self.full_name}'
+        return f"&{self.full_name}"
 
     @property
     def type(self, show_calling_convention: bool = False):
@@ -75,18 +78,24 @@ class Function(Symbol):
         return f"{self.ret_type}({calling} *)({args})"
 
     @property
+    def args_signature(self):
+        return ",".join(f"{arg.type} {arg.name}" for arg in self.args)
+
+    @property
     def signature(self):
-        arg_str = ",".join(f'{arg.type} {arg.name}' for arg in self.args)
-        s = f"{self.full_name} ({arg_str})"
+        s = f"{self.full_name} ({self.args_signature})"
         return s
 
     def __str__(self):
         return self.signature
 
+    def __hash__(self):
+        return hash(self.signature)
 
-@dataclass(repr=False)
+
+@dataclass(repr=False, unsafe_hash=True)
 class Method(Function):
-    ret_type: str = ''
+    ret_type: str = ""
     parent: "Class" = None
     access: str = "public"
     is_virtual: bool = False
@@ -105,17 +114,35 @@ class Method(Function):
             parent_prefix = f"{self.parent.full_name}::"
         return f"{self.ret_type}({calling}{parent_prefix}*)({args})"
 
+    def declaration_in_class(self):
+        return "{access}: {virtual}{static}{ret} {name}({args}){pure_virtual}".format(
+            access=f"{self.access}",
+            virtual="virtual " if self.is_virtual else "",
+            static="static " if self.is_static else "",
+            ret=self.ret_type,
+            name=self.name,
+            args=self.args_signature,
+            pure_virtual=" = 0" if self.is_pure_virtual else "",
+        )
+
+    def declaration_global(self):
+        return "{ret} {scope}{name}({args}){pure_virtual}".format(
+            ret=self.ret_type,
+            name=self.name,
+            scope=f"{self.parent.full_name}::" if self.parent else "",
+            args=self.args_signature,
+            pure_virtual=" = 0" if self.is_pure_virtual else "",
+        )
+
     @property
     def signature(self):
-        return (
-            "{} {}{} {}::".format(
-                self.access,
-                "virtual" if self.is_virtual else "",
-                "static" if self.is_static else "",
-                self.parent.name,
-            ) +
-            super().signature +
-            (" = 0" if self.is_pure_virtual else "")
+        return "{access} {virtual}{static} {scope}{signature}{pure_virtual}".format(
+            access=self.access,
+            virtual="virtual" if self.is_virtual else "",
+            static="static" if self.is_static else "",
+            scope=f"{self.parent.name}::" if self.parent else "",
+            signature=super().signature,
+            pure_virtual=" = 0" if self.is_pure_virtual else "",
         )
 
     def __str__(self):
@@ -134,6 +161,12 @@ class Namespace(Symbol):
         default_factory=(lambda: defaultdict(list))
     )
     namespaces: Dict[str, "Namespace"] = field(default_factory=dict)
+
+    # implementations
+    forward_declares: list[str] = field(default_factory=list)
+    using_namespaces: list[str] = field(default_factory=list)
+    method_impls: list[Method] = field(default_factory=list)
+    function_impls: list[Function] = field(default_factory=list)
 
     def extend(self, other: "Namespace"):
         self.enums.update(other.enums)
@@ -169,20 +202,42 @@ class Class(Namespace):
 
     is_polymorphic: bool = False  # has virtual methods
 
-    can_generate_wrapper: bool = True  # generate a wrapper if it is a polymorphic class
+    can_generate_wrapper: bool = (
+        True  # generate a wrapper if it is a polymorphic class
+    )
 
     def __str__(self):
         return "class " + self.name
 
 
 @dataclass(repr=False)
+class File(Namespace):
+    directory: str = ""
+    relative_includes: list[str] = field(default_factory=list)
+    absolute_includes: list[str] = field(default_factory=list)
+    pragma_once: bool = False
+
+    @property
+    def filename(self) -> str:
+        return self.name
+
+    @property
+    def full_name(self):
+        return ""
+
+    @property
+    def filepath(self) -> str:
+        return f"{self.directory}/{self.name}"
+
+
+@dataclass(repr=False)
 class AnonymousUnion(Class):
-    scope_name: str = ''  # same as its variable_name in parent
+    scope_name: str = ""  # same as its variable_name in parent
 
     @property
     def full_name(self):
         if self.scope_name:
-            return f'decltype({self.parent.full_name}::{self.scope_name})'
+            return f"decltype({self.parent.full_name}::{self.scope_name})"
         else:
             return self.parent.full_name
 
